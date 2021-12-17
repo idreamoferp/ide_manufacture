7fcf71260818710f577a4ad3e6cd9a565c18ebdb
Justin Mangini <justin@diyautospec.com>
2021-12-17 15:10:19 -0700

    [mrp_routeing_knowledge] create module

---
+++ mrp_routeing_knowledge/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "MRP Routing Knowledge",
+    "version": "0.1",
+    "author": "Justin Mangini",
+    "summary": "Replaces routing worksheet with a knowledge page",
+    "website": "",
+    "category": "Manufacturing",
+    "depends": ["mrp", "knowledge"],
+    "data": ["views/bom_template.xml",],
+    "license": "LGPL-3",
+    "installable": True,
+}
